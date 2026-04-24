b93de295de61b116d3dc4928bac91696d523316a
ano3 <ano333333github@gmail.com>
2026-04-24 23:43:06 +0900

    fix: iOS系では画像をダウンロードせず新タブで開く

--- src/helpers/captureAndDownload.ts
+++ src/helpers/captureAndDownload.ts
@@ -11,18 +11,24 @@ export async function captureAndDownload(
 ): Promise<void> {
 	const canvas = await html2canvas(target);
 
+	const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
 	await new Promise<void>((resolve) => {
 		canvas.toBlob((blob) => {
 			if (blob) {
 				const url = URL.createObjectURL(blob);
-				const a = document.createElement("a");
-				a.href = url;
-				a.download = filename;
-				a.style.display = "none";
-				document.body.appendChild(a);
-				a.click();
-				document.body.removeChild(a);
-				URL.revokeObjectURL(url);
+				if (isIOS) {
+					window.open(url, "_blank");
+				} else {
+					const a = document.createElement("a");
+					a.href = url;
+					a.download = filename;
+					a.style.display = "none";
+					document.body.appendChild(a);
+					a.click();
+					document.body.removeChild(a);
+					URL.revokeObjectURL(url);
+				}
 			}
 			resolve();
 		});
